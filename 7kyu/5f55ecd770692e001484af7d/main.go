e969dca51abd9ac495377dd9cb2ac7738242e070
Sergey Khorbin <khorbin96@gmail.com>
2024-05-03 20:17:21 +0200

    completed 5f55ecd770692e001484af7d

---
+++ 7kyu/5f55ecd770692e001484af7d/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(Mirror([]int{}))                         // {}))
+	fmt.Println(Mirror([]int{1}))                        // {1}))
+	fmt.Println(Mirror([]int{2, 1}))                     // {1, 2, 1}))
+	fmt.Println(Mirror([]int{1, 3, 2}))                  // {1, 2, 3, 2, 1}))
+	fmt.Println(Mirror([]int{-8, 42, 18, 0, -16}))       // {-16, -8, 0, 18, 42, 18, 0, -8, -16}))
+	fmt.Println(Mirror([]int{-5, 10, 8, 10, 2, -3, 10})) // {-5, -3, 2, 8, 10, 10, 10, 10, 10, 8, 2, -3, -5}))
+}
+
+func Mirror(data []int) []int {
+	var response = make([]int, len(data), len(data)*2)
+	copy(response, data)
+	sort.Ints(response)
+
+	for i := len(response) - 2; i >= 0; i = i - 1 {
+		response = append(response, response[i])
+	}
+	return response
+}
+
+// https://www.codewars.com/kata/5f55ecd770692e001484af7d
+//
+//
+// Too long, didn't read
+// You get a list of integers, and you have to write a function mirror that returns the
+// "mirror" (or symmetric) version of this list: i.e. the middle element is the greatest,
+// then the next greatest on both sides, then the next greatest, and so on...
+
+// More info
+// The list will always consist of integers in range -1000..1000 and will vary in size
+// between 0 and 10000. Your function should not mutate the input array, and this will be
+// tested (where applicable). Notice that the returned list will always be of odd size,
+// since there will always be a definitive middle element.
+
+// Examples
+// []  -->  []
+// [1]  -->  [1]
+// [2, 1]  -->  [1, 2, 1]
+// [1, 3, 2]  -->  [1, 2, 3, 2, 1]
+// [-8, 42, 18, 0, -16]  -->  [-16, -8, 0, 18, 42, 18, 0, -8, -16]
+// [-3, 15, 8, -1, 7, -1] --> [-3, -1, -1, 7, 8, 15, 8, 7, -1, -1, -3]
+// [-5, 10, 8, 10, 2, -3, 10] --> [-5, -3, 2, 8, 10, 10, 10, 10, 10, 8, 2, -3, -5]
+// Good luck!
