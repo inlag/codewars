1e40a603ae41253b1c5447e63089e67f15f65c88
Sergey Khorbin <khorbin96@gmail.com>
2024-05-10 20:18:49 +0200

    completed 585a033e3a36cdc50a00011c

---
+++ 7kyu/585a033e3a36cdc50a00011c/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(FreqSeq("hello world", "-"))
+}
+func FreqSeq(str string, sep string) string {
+	var (
+		tmp = make(map[byte]int)
+		res = make([]string, len(str))
+	)
+
+	for i := 0; i <= len(str)-1; i++ {
+		tmp[str[i]]++
+	}
+
+	for i := 0; i <= len(str)-1; i++ {
+		res[i] = strconv.Itoa(tmp[str[i]])
+	}
+
+	return strings.Join(res, sep)
+}
+
+// https://www.codewars.com/kata/585a033e3a36cdc50a00011c
+//
+// Your task is to return an output string that translates an input
+// string s by replacing each character in s with a number representing
+// the number of times that character occurs in s and separating each number with the sep character(s).
+
+// Example (s, sep --> Output)
+
+// "hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+// "19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+// "^^^**$"     , "x" --> "3x3x3x2x2x1"
